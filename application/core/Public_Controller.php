a440c7d30fd979d400135eb9d2406cb2c1f5ae92
a0935756869 <a0935756869@gmail.com>
2022-07-25 13:38:03 +0800

    調整-core&helper

---
+++ application/core/Public_Controller.php
@@ -0,0 +1,43 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Public_Controller extends MY_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		if(strpos($_SERVER['HTTP_HOST'],'localhost') !== false){
+			//
+		} else {
+			if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+			    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+			    header('HTTP/1.1 301 Moved Permanently');
+			    header('Location: ' . $location);
+			    exit;
+			}
+		}
+		// if (!$this->ion_auth->logged_in())
+		// {
+		// 	$this->session->sess_destroy();
+		// 	redirect('login', 'refresh');
+		// }
+        // $this->data['current_user'] = $this->ion_auth->user()->row();
+        $this->load->library('Ajax_pagination');
+        $this->data['admin_user_menu'] = '';
+        // if ($this->ion_auth->logged_in()) {
+        if(!empty($this->session->userdata('user_id'))){
+        	$this->data['address'] = $this->mysql_model->_select('users_address', 'user_id', $this->session->userdata('user_id'));
+        }
+        // }
+        $this->perPage = get_setting_general('per_page');
+
+        if($this->input->get('botID')){
+	        $data = array(
+	           'botID' => $this->input->get('botID'),
+	        );
+	        $this->session->set_userdata($data);
+        }
+
+		$this->data['include_style'] = $this->load->view('templates/_parts/style.php', NULL, TRUE);
+		$this->data['include_script'] = $this->load->view('templates/_parts/script.php', NULL, TRUE);
+	}
+}
