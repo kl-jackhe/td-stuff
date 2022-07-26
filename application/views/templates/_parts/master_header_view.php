46b71e2580dfec750af8270d4997f0550f04d881
jack <a0985057475@gmail.com>
2022-07-26 18:43:53 +0800

    header OK

--- application/views/templates/_parts/master_header_view.php
+++ application/views/templates/_parts/master_header_view.php
@@ -86,42 +86,111 @@
         height: 25px;
         text-align: center;
     }
+    .top_logo_style {
+        position: absolute;
+        transform: translate(-50%, -50%);
+        left: 50%;
+        top: 40%;
+    }
+    .nav_user_style {
+        position:absolute;
+        right: 35px;
+    }
+    .nav_user_style li {
+        padding: 5px;
+    }
+    .nav_user_login_edit {
+        border: 1px solid #534431;
+        color: #808080 !important;
+        padding: 2px 12px 2px 12px !important;
+    }
+    .nav_user_register_logout {
+        background: #534431;
+        color: #fff !important;
+        padding: 2px 12px 2px 12px !important;
+    }
+    .fixed_icon_style {
+        left: auto;right: 25px;bottom: 60px;
+    }
+    .fixed_icon_style i {
+        font-size: 48px;
+    }
+    @media (min-width: 768px) and (max-width: 991.98px) {
+        .top_logo_style {
+            position: relative;
+            transform: none;
+            left: -10px;
+            top: 0;
+        }
+        .nav_user_style {
+            position: relative;
+            right: 0;
+            width: 12%;
+            left: 10px;
+        }
+    }
+    @media (max-width: 767px) {
+        .navbar-toggler {
+            padding: 0.45rem 0.75rem;
+            left: 0px;
+            top: 5px;
+            position: relative;
+        }
+        .m_padding {
+            padding: 0px !important;
+        }
+        .top_logo_style {
+            position: relative;
+            transform: none;
+            left: -35%;
+            top: 0;
+        }
+        .nav_user_style {
+            position: relative;
+            right: -10px;
+            width: 30%;
+        }
+        .m_hr_border {
+            padding-right: 15px !important;
+            padding-left: 15px !important;
+        }
+    }
 </style>
 
 <body>
     <div class="body h-100">
-        <div class="fixed-bottom" style="left: auto;right: 25px;bottom: 60px;">
+        <div class="fixed-bottom fixed_icon_style">
             <div id="fa-facebook-square" class="py-1">
                 <a href="#">
-                    <i class="fa-brands fa-facebook-square" style="font-size: 48px;"></i>
+                    <i class="fa-brands fa-facebook-square"></i>
                 </a>
             </div>
             <div id="fa-line" class="py-1">
                 <a target="_blank" href="https://line.me/R/ti/p/@504bdron">
-                    <i class="fa-brands fa-line" style="font-size: 48px;"></i>
+                    <i class="fa-brands fa-line"></i>
                 </a>
             </div>
             <div id="fa-bag-shopping" class="py-1">
                 <a href="#" data-toggle="modal" data-target="#my_cart" onclick="get_mini_cart();">
                     <div id="cart-qty"><span style="color: #000;">0</span></div>
-                    <i class="fa-solid fa-bag-shopping" style="font-size: 48px;"></i>
+                    <i class="fa-solid fa-bag-shopping"></i>
                 </a>
             </div>
             <div id="fa-angles-up" class="py-1 text-center" style="display:none;">
                 <a href="#" style="color:black;">
-                    <i class="fa-solid fa-angles-up" style="font-size: 48px;"></i>
+                    <i class="fa-solid fa-angles-up"></i>
                 </a>
             </div>
         </div>
         <header id="header" class="header-narrow header-semi-transparent header-transparent-sticky-deactive custom-header-transparent-bottom-border">
             <div class="header-body">
-                <div class="header-container" style="width: 100%;">
-                    <div class="header-row" style="padding-left:25px;padding-right:25px;">
+                <div class="header-container container m_padding">
+                    <div class="header-row py-4 m_padding">
                         <nav class="navbar navbar-expand-lg navbar-light">
                             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
-                            <a href="<?php echo base_url() ?>" style="position: absolute;transform: translate(-50%, -50%);left: 50%;top: 50%;">
+                            <a href="<?php echo base_url() ?>" class="top_logo_style">
                                 <img class="img-fluid" src="/assets/uploads/<?php echo get_setting_general('logo'); ?>">
                             </a>
                             <div class="collapse navbar-collapse" id="navbarToggler">
@@ -130,148 +199,37 @@
                                         <a class="nav-link" href="<?php echo base_url() ?>">首頁</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="/product">商城</a>
+                                        <a class="nav-link" href="#">關於龍寶</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="/checkout">結帳</a>
-                                    </li>
-                                    <?php if (empty($this->session->userdata('user_id'))) {?>
-                                    <li class="nav-item">
-                                        <a href="/login" class="btn nav-link" style="border: 1px solid #00BFD5; color: #00BFD5; margin-bottom: 10px;">登入</a>
+                                        <a class="nav-link" href="#">商品分類</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a href="/register" class="btn register-btn nav-link" style="background: #00BFD5; color: #fefefe;">註冊</a>
+                                        <a class="nav-link" href="/product">全商品</a>
                                     </li>
-                                    <?}else{?>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="/auth/edit_user">我的帳戶</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="/logout">登出</a>
-                                    </li>
-                                    <?}?>
-                                    <!-- <div class="dropdown">
-                                        <?php if (!empty($this->session->userdata('user_id'))) {?>
-                                        <div id="bbb" class="btn btn-secondary dropdown-toggle" data-toggle="collapse" data-target=".dropdown-menu" aria-expanded="false">
-                                            <?php echo $this->ion_auth->user()->row()->full_name; ?>
-                                        </div>
-                                        <ul class="dropdown-menu collapse">
-                                            <li>
-                                                <a href="/auth/edit_user">個人資料</a>
-                                            </li>
-                                            <li>
-                                                <a href="/coupon">優惠券管理</a>
-                                            </li>
-                                            <li>
-                                                <a href="/order">訂單管理</a>
-                                            </li>
-                                            <li class="worker">
-                                                <a href="javascript:;" onclick="my_addess_model();">常用地址</a>
-                                            </li>
-                                            <li>
-                                                <a href="/logout">登出</a>
-                                            </li>
-                                        </ul>
+                                    <div class="row nav_user_style">
+                                        <?php if (empty($this->session->userdata('user_id'))) {?>
+                                        <li class="nav-item">
+                                            <a href="/login" class="btn nav-link nav_user_login_edit">登入</a>
+                                        </li>
+                                        <li class="nav-item" >
+                                            <a href="/register" class="btn register-btn nav-link nav_user_register_logout">註冊</a>
+                                        </li>
+                                        <?}else{?>
+                                        <li class="nav-item" >
+                                            <a class="btn nav-link nav_user_login_edit" href="/auth/edit_user">我的帳戶</a>
+                                        </li>
+                                        <li class="nav-item" >
+                                            <a class="btn nav-link nav_user_register_logout" href="/logout">登出</a>
+                                        </li>
+                                        <?}?>
                                     </div>
-                                    <?php } else {?>
-                                    <?php if ($this->uri->segment(1) == '' || $this->uri->segment(1) == 'login' || $this->uri->segment(1) == 'register') {?>
-                                    <li class="nav-item">
-                                        <a href="/login" class="btn" style="border: 1px solid #00BFD5; color: #00BFD5; margin-bottom: 10px;">登入</a>
-                                    </li>
-                                    <li class="nav-item" style="margin-left: 0px;">
-                                        <a href="/register" class="btn register-btn" style="background: #00BFD5; color: #fefefe;">註冊</a>
-                                    </li>
-                                    <?php }}?> -->
                                 </ul>
-                                <!-- <form class="form-inline my-2 my-lg-0">
-                                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                                </form> -->
                             </div>
                         </nav>
-                        <div class="px-4">
+                        <div class="px-4 m_hr_border">
                             <hr>
                         </div>
-                        <!--  <div class="header-column">
-                            <div class="header-row">
-                                <div class="header-nav">
-                        <button type="button" class="btn header-btn-collapse-nav" data-toggle="collapse" data-target=".header-nav-main">
-                                        <i class="fa fa-navicon"></i>
-                                    </button>
-                        <div class="header-nav-main collapse" aria-expanded="true">
-                                        <nav>
-                                            <ul class="nav nav-pills" id="mainNav">
-                                                <li class="worker">
-                                                    <a href="/about/cross_industry_alliance">
-                                                        異業合作
-                                                    </a>
-                                                </li>
-                                                <li class="worker">
-                                                    <a href="/about/shop_alliance">
-                                                        店家合作
-                                                    </a>
-                                                </li>
-                                                <?php if ($this->ion_auth->logged_in()) {?>
-                                                <li class="worker">
-                                                    <a href="javascript:;" onclick="my_addess_model();">
-                                                        常用地址
-                                                    </a>
-                                                </li>
-                                                <?php }?>
-                                                <li class="message">
-                                                    <a href="/posts">
-                                                        最新活動
-                                                    </a>
-                                                </li>
-                                                <li class="message" style="margin-right: 40px;">
-                                                    <a href="/about/brand">
-                                                        關於我們
-                                                    </a>
-                                                </li>
-                                                <li class="dropdown">
-                                                    <?php if (!empty($this->session->userdata('user_id'))) {?>
-                                                    <div id="bbb" class="btn btn-block mb-sm" data-toggle="collapse" data-target=".dropdown-menu" aria-expanded="true">
-                                                        <i class="fa fa-angle-up pull-left visible-xs"></i>
-                                                        <?php echo $this->ion_auth->user()->row()->full_name; ?>
-                                                    </div>
-                                                    <ul class="dropdown-menu collapse">
-                                                        <li>
-                                                            <a href="/auth/edit_user">個人資料</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="/coupon">優惠券管理</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="/order">訂單管理</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="/logout">登出</a>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                                <?php } else {?>
-                                                <?php if ($this->uri->segment(1) == '' || $this->uri->segment(1) == 'login' || $this->uri->segment(1) == 'register') {?>
-                                                <li>
-                                                    <a href="/login" class="btn" style="border: 1px solid #00BFD5; color: #00BFD5; margin-bottom: 10px;">登入</a>
-                                                </li>
-                                                <li style="margin-left: 0px;">
-                                                    <a href="/register" class="btn register-btn" style="background: #00BFD5; color: #fefefe;">註冊</a>
-                                                </li>
-                                                <?php } else {?>
-                                                <li>
-                                                    <a href="#" class="btn" onclick="login_model()" style="border: 1px solid #00BFD5; color: #00BFD5; margin-bottom: 10px;">登入</a>
-                                                </li>
-                                                <li style="margin-left: 0px;">
-                                                    <a href="#" class="btn register-btn" onclick="register_model()" style="background: #00BFD5; color: #fefefe;">註冊</a>
-                                                </li>
-                                                <?php }?>
-                                                <?php }?>
-                                            </ul>
-                                        </nav>
-                                    </div>
-                            </div>
-                            </div>
-                        </div> -->
                     </div>
                 </div>
             </div>
