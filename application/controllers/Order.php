325dc2624713eb0e83f8fe6fc4b7d669f98c9064
jack <a0985057475@gmail.com>
2022-08-04 13:53:49 +0800

    fix order load username

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -2,99 +2,94 @@
 
 class Order extends Public_Controller {
 
-    function __construct()
-    {
-        parent::__construct();
-        $this->load->model('home_model');
-        $this->load->model('order_model');
-        if (!$this->ion_auth->logged_in()) {
-            redirect('login', 'refresh');
-        }
-    }
+	function __construct() {
+		parent::__construct();
+		$this->load->model('home_model');
+		$this->load->model('order_model');
+		if (!$this->ion_auth->logged_in()) {
+			redirect('login', 'refresh');
+		}
+	}
 
-    public function index()
-    {
-        $this->data['page_title'] = '訂單';
+	public function index() {
+		$this->data['page_title'] = '訂單';
 
-        $this->data['orders'] = array();
-        if(isset($this->current_user->id)){
-            $this->data['orders'] = $this->order_model->get_customer_order($this->current_user->id);
-        }
-        $this->render('order/index');
-    }
+		$this->data['orders'] = array();
+		// if (isset($this->current_user->id)) {
+		// 	$this->data['orders'] = $this->order_model->get_customer_order($this->current_user->id);
+		// }
+		if (isset($this->current_user->username)) {
+			$this->data['orders'] = $this->order_model->get_customer_phone_order($this->current_user->username);
+		}
+		$this->render('order/index');
+	}
 
-    public function view($id)
-    {
-        $id = decode($id);
-        $this->data['page_title'] = '查看訂單';
-        $this->data['order'] = $this->mysql_model->_select('orders', 'order_id', $id, 'row');
-        $this->data['order_item'] = $this->mysql_model->_select('order_item', 'order_id', $id);
-        $this->load->view('order/view', $this->data);
-    }
+	public function view($id) {
+		$id = decode($id);
+		$this->data['page_title'] = '查看訂單';
+		$this->data['order'] = $this->mysql_model->_select('orders', 'order_id', $id, 'row');
+		$this->data['order_item'] = $this->mysql_model->_select('order_item', 'order_id', $id);
+		$this->load->view('order/view', $this->data);
+	}
 
-    public function checkout()
-    {
-        $this->data['page_title'] = '結帳';
-        $this->data['banner'] = $this->home_model->GetBanner();
-        $this->render('store/checkout');
-    }
+	public function checkout() {
+		$this->data['page_title'] = '結帳';
+		$this->data['banner'] = $this->home_model->GetBanner();
+		$this->render('store/checkout');
+	}
 
-    public function atm()
-    {
-        $this->data['page_title'] = 'ATM付款';
-        $this->data['banner'] = $this->home_model->GetBanner();
-        $this->render('store/checkout-atm-callback');
-    }
+	public function atm() {
+		$this->data['page_title'] = 'ATM付款';
+		$this->data['banner'] = $this->home_model->GetBanner();
+		$this->render('store/checkout-atm-callback');
+	}
 
-    public function credit()
-    {
-        $this->data['page_title'] = '信用卡付款';
-        $this->data['banner'] = $this->home_model->GetBanner();
-        $this->render('store/checkout-credit-callback');
-    }
+	public function credit() {
+		$this->data['page_title'] = '信用卡付款';
+		$this->data['banner'] = $this->home_model->GetBanner();
+		$this->render('store/checkout-credit-callback');
+	}
 
-    public function cancel($id)
-    {
-        $data = array(
-            'order_step' => 'cancel',
-            'order_pay_status' => 'cancel',
-            'order_void' => '1',
-            'updater_id' => $this->ion_auth->user()->row()->id,
-            'updated_at' => date('Y-m-d H:i:s'),
-        );
-        $this->db->where('order_id', $id);
-        $this->db->update('orders', $data);
-        //
-        $item_data = array(
-            'order_item_void' => '1',
-        );
-        $this->db->where('order_id', $id);
-        $this->db->update('order_item', $item_data);
+	public function cancel($id) {
+		$data = array(
+			'order_step' => 'cancel',
+			'order_pay_status' => 'cancel',
+			'order_void' => '1',
+			'updater_id' => $this->ion_auth->user()->row()->id,
+			'updated_at' => date('Y-m-d H:i:s'),
+		);
+		$this->db->where('order_id', $id);
+		$this->db->update('orders', $data);
+		//
+		$item_data = array(
+			'order_item_void' => '1',
+		);
+		$this->db->where('order_id', $id);
+		$this->db->update('order_item', $item_data);
 
-        $this->session->set_flashdata('message', '取消訂單成功！');
-        redirect($_SERVER['HTTP_REFERER']);
-    }
+		$this->session->set_flashdata('message', '取消訂單成功！');
+		redirect($_SERVER['HTTP_REFERER']);
+	}
 
-    public function void($id)
-    {
-        $data = array(
-            'order_step' => 'void',
-            'order_pay_status' => 'return',
-            'order_void' => '1',
-            'updater_id' => $this->ion_auth->user()->row()->id,
-            'updated_at' => date('Y-m-d H:i:s'),
-        );
-        $this->db->where('order_id', $id);
-        $this->db->update('orders', $data);
-        //
-        $item_data = array(
-            'order_item_void' => '1',
-        );
-        $this->db->where('order_id', $id);
-        $this->db->update('order_item', $item_data);
+	public function void($id) {
+		$data = array(
+			'order_step' => 'void',
+			'order_pay_status' => 'return',
+			'order_void' => '1',
+			'updater_id' => $this->ion_auth->user()->row()->id,
+			'updated_at' => date('Y-m-d H:i:s'),
+		);
+		$this->db->where('order_id', $id);
+		$this->db->update('orders', $data);
+		//
+		$item_data = array(
+			'order_item_void' => '1',
+		);
+		$this->db->where('order_id', $id);
+		$this->db->update('order_item', $item_data);
 
-        $this->session->set_flashdata('message', '退單成功！');
-        redirect($_SERVER['HTTP_REFERER']);
-    }
+		$this->session->set_flashdata('message', '退單成功！');
+		redirect($_SERVER['HTTP_REFERER']);
+	}
 
 }
--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -2,123 +2,127 @@
 
 class Order_model extends CI_Model {
 
-    function getRows($params = array()){
-        $this->db->select('*');
-        $this->db->from('orders');
-        //filter data by searched keywords
-        if(!empty($params['search']['keywords'])){
-            $this->db->group_start();
-            $this->db->like('order_number',$params['search']['keywords']);
-            $this->db->or_like('customer_name',$params['search']['keywords']);
-            $this->db->or_like('customer_phone',$params['search']['keywords']);
-            $this->db->group_end();
-        }
-        if(!empty($params['search']['category'])){
-            $this->db->where('order_step',$params['search']['category']);
-        }
-        if(!empty($params['search']['category2'])){
-            // $this->db->where('order_step',$params['search']['category2']);
-            // $this->db->where('order_delivery_place',$params['search']['category']);
-        }
-        if(!empty($params['search']['start_date'])){
-            $this->db->where('order_date >=', $params['search']['start_date']);
-        }
-        if(!empty($params['search']['end_date'])){
-            $this->db->where('order_date <=', $params['search']['end_date']);
-        }
-        //sort data by ascending or desceding order
-        if(!empty($params['search']['sortBy'])){
-            $this->db->order_by('order_date', $params['search']['sortBy']);
-        } else {
-            $this->db->order_by('order_date', 'desc');
-        }
-        // if(!empty($params['search']['status'])){
-        //     $this->db->where('order_status',$params['search']['status']);
-        // } else {
-        //     $this->db->where('order_status', '1');
-        // }
-        //set start and limit
-        if(array_key_exists("start",$params) && array_key_exists("limit",$params)){
-            $this->db->limit($params['limit'],$params['start']);
-        }elseif(!array_key_exists("start",$params) && array_key_exists("limit",$params)){
-            $this->db->limit($params['limit']);
-        }
-        if(array_key_exists("returnType",$params) && $params['returnType'] == 'count'){
-            $result = $this->db->count_all_results();
-        }else{
-            //get records
-            $query = $this->db->get();
-            //return fetched data
-            $result = ($query->num_rows() > 0)?$query->result_array():false;
-        }
-        return $result;
-    }
+	function getRows($params = array()) {
+		$this->db->select('*');
+		$this->db->from('orders');
+		//filter data by searched keywords
+		if (!empty($params['search']['keywords'])) {
+			$this->db->group_start();
+			$this->db->like('order_number', $params['search']['keywords']);
+			$this->db->or_like('customer_name', $params['search']['keywords']);
+			$this->db->or_like('customer_phone', $params['search']['keywords']);
+			$this->db->group_end();
+		}
+		if (!empty($params['search']['category'])) {
+			$this->db->where('order_step', $params['search']['category']);
+		}
+		if (!empty($params['search']['category2'])) {
+			// $this->db->where('order_step',$params['search']['category2']);
+			// $this->db->where('order_delivery_place',$params['search']['category']);
+		}
+		if (!empty($params['search']['start_date'])) {
+			$this->db->where('order_date >=', $params['search']['start_date']);
+		}
+		if (!empty($params['search']['end_date'])) {
+			$this->db->where('order_date <=', $params['search']['end_date']);
+		}
+		//sort data by ascending or desceding order
+		if (!empty($params['search']['sortBy'])) {
+			$this->db->order_by('order_date', $params['search']['sortBy']);
+		} else {
+			$this->db->order_by('order_date', 'desc');
+		}
+		// if(!empty($params['search']['status'])){
+		//     $this->db->where('order_status',$params['search']['status']);
+		// } else {
+		//     $this->db->where('order_status', '1');
+		// }
+		//set start and limit
+		if (array_key_exists("start", $params) && array_key_exists("limit", $params)) {
+			$this->db->limit($params['limit'], $params['start']);
+		} elseif (!array_key_exists("start", $params) && array_key_exists("limit", $params)) {
+			$this->db->limit($params['limit']);
+		}
+		if (array_key_exists("returnType", $params) && $params['returnType'] == 'count') {
+			$result = $this->db->count_all_results();
+		} else {
+			//get records
+			$query = $this->db->get();
+			//return fetched data
+			$result = ($query->num_rows() > 0) ? $query->result_array() : false;
+		}
+		return $result;
+	}
 
-    function find_all_order($params = array()){
-        $this->db->select('*');
-        $this->db->from('orders');
-        $this->db->like('created_at', date('Y-m-d'));
-        //filter data by searched keywords
-        if(!empty($params['search']['keywords'])){
-            $this->db->like('order_id',$params['search']['keywords']);
-            //$this->db->or_like('order_eat_type',$params['search']['keywords']);
-            //$this->db->or_like('invoice_randcode',$params['search']['keywords']);
-            //$this->db->like('created_at',$params['search']['keywords'], 'after');
-        }
-        //sort data by ascending or desceding order
-        if(!empty($params['search']['sortBy'])){
-            $this->db->order_by('order_id',$params['search']['sortBy']);
-        }else{
-            $this->db->order_by('order_id','desc');
-        }
-        if(!empty($params['search']['category'])){
-            $this->db->where('order_eat_type',$params['search']['category']);
-        }
-        if(!empty($params['search']['status'])){
-            $this->db->where('order_status',$params['search']['status']);
-        } else {
-            $this->db->where('order_status', '1');
-        }
-        //set start and limit
-        // if(array_key_exists("start",$params) && array_key_exists("limit",$params)){
-        //     $this->db->limit($params['limit'],$params['start']);
-        // }elseif(!array_key_exists("start",$params) && array_key_exists("limit",$params)){
-        //     $this->db->limit($params['limit']);
-        // }
-        //get records
-        $query = $this->db->get();
-        //return fetched data
-        return ($query->num_rows() > 0)?$query->result_array():false;
-    }
+	function find_all_order($params = array()) {
+		$this->db->select('*');
+		$this->db->from('orders');
+		$this->db->like('created_at', date('Y-m-d'));
+		//filter data by searched keywords
+		if (!empty($params['search']['keywords'])) {
+			$this->db->like('order_id', $params['search']['keywords']);
+			//$this->db->or_like('order_eat_type',$params['search']['keywords']);
+			//$this->db->or_like('invoice_randcode',$params['search']['keywords']);
+			//$this->db->like('created_at',$params['search']['keywords'], 'after');
+		}
+		//sort data by ascending or desceding order
+		if (!empty($params['search']['sortBy'])) {
+			$this->db->order_by('order_id', $params['search']['sortBy']);
+		} else {
+			$this->db->order_by('order_id', 'desc');
+		}
+		if (!empty($params['search']['category'])) {
+			$this->db->where('order_eat_type', $params['search']['category']);
+		}
+		if (!empty($params['search']['status'])) {
+			$this->db->where('order_status', $params['search']['status']);
+		} else {
+			$this->db->where('order_status', '1');
+		}
+		//set start and limit
+		// if(array_key_exists("start",$params) && array_key_exists("limit",$params)){
+		//     $this->db->limit($params['limit'],$params['start']);
+		// }elseif(!array_key_exists("start",$params) && array_key_exists("limit",$params)){
+		//     $this->db->limit($params['limit']);
+		// }
+		//get records
+		$query = $this->db->get();
+		//return fetched data
+		return ($query->num_rows() > 0) ? $query->result_array() : false;
+	}
 
-    public function find_other_order($type)
-    {
-        //$this->db->join('purchase_item', 'purchase_item.purchase_id = purchase.purchase_id');
-        $this->db->where('order_status', '1');
-        $this->db->where('order_eat_type', $type);
-        $this->db->order_by('order_id','desc');
-        $query = $this->db->get('pos_other_order');
-        return ($query->num_rows() > 0)?$query->result_array():false;
-    }
+	public function find_other_order($type) {
+		//$this->db->join('purchase_item', 'purchase_item.purchase_id = purchase.purchase_id');
+		$this->db->where('order_status', '1');
+		$this->db->where('order_eat_type', $type);
+		$this->db->order_by('order_id', 'desc');
+		$query = $this->db->get('pos_other_order');
+		return ($query->num_rows() > 0) ? $query->result_array() : false;
+	}
 
-    public function get_customer_order($id = 0)
-    {
-        if($id==0){
-            return false;
-        }
-        $this->db->where('customer_id', $id);
-        $this->db->order_by('order_id','desc');
-        $query = $this->db->get('orders');
-        return ($query->num_rows() > 0)?$query->result_array():false;
-    }
+	public function get_customer_order($id = 0) {
+		if ($id == 0) {
+			return false;
+		}
+		$this->db->where('customer_id', $id);
+		$this->db->order_by('order_id', 'desc');
+		$query = $this->db->get('orders');
+		return ($query->num_rows() > 0) ? $query->result_array() : false;
+	}
 
-    public function get_order_item_with_product($id)
-    {
-        $this->db->join('product', 'product.product_id = order_item.product_id');
-        $this->db->where('order_id', $id);
-        // $this->db->order_by('order_id','desc');
-        $query = $this->db->get('order_item');
-        return ($query->num_rows() > 0)?$query->result_array():false;
-    }
+	public function get_customer_phone_order($id) {
+		$this->db->where('customer_phone', $id);
+		$this->db->order_by('order_id', 'desc');
+		$query = $this->db->get('orders');
+		return ($query->num_rows() > 0) ? $query->result_array() : false;
+	}
+
+	public function get_order_item_with_product($id) {
+		$this->db->join('product', 'product.product_id = order_item.product_id');
+		$this->db->where('order_id', $id);
+		// $this->db->order_by('order_id','desc');
+		$query = $this->db->get('order_item');
+		return ($query->num_rows() > 0) ? $query->result_array() : false;
+	}
 
 }
