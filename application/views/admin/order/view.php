0b6ba1019a22108b649574a7346f5dbdfee91fb5
jack <a0985057475@gmail.com>
2022-08-09 11:41:21 +0800

    fix admin order view show

--- application/views/admin/order/view.php
+++ application/views/admin/order/view.php
@@ -1,3 +1,16 @@
+<style>
+    img {
+        width: 100%;
+    }
+    .front_title {
+        font-size: 18px;
+    }
+    .money_size {
+        color: #dd0606;
+        font-weight: bold;
+        font-size: 24px;
+    }
+</style>
 <div class="row">
     <?php // $attributes = array('class' => 'download', 'id' => 'download');?>
     <?php // echo form_open('admin/order/update/' . $order['order_id'], $attributes); ?>
@@ -16,11 +29,11 @@
                     <div class="form-group">
                         <h4>訂單狀態：<?php echo get_order_step($order['order_step']) ?></h4>
                     </div>
-                    <p>客戶名稱：<?php echo $order['customer_name'] ?></p>
-                    <p>客戶電話：<?php echo $order['customer_phone'] ?></p>
-                    <p>客戶信箱：<?php echo $order['customer_email'] ?></p>
-                    <p>訂單編號：<?php echo $order['order_number'] ?></p>
-                    <p>訂單日期：<?php echo $order['order_date'] ?></p>
+                    <p class="front_title">客戶名稱：<?php echo $order['customer_name'] ?></p>
+                    <p class="front_title">客戶電話：<?php echo $order['customer_phone'] ?></p>
+                    <p class="front_title">客戶信箱：<?php echo $order['customer_email'] ?></p>
+                    <p class="front_title">訂單編號：<?php echo $order['order_number'] ?></p>
+                    <p class="front_title">訂單日期：<?php echo $order['order_date'] ?></p>
 
                     <table class="table table-bordered">
                         <tr>
@@ -28,44 +41,66 @@
                                 <table class="table table-bordered table-hover">
                                     <thead>
                                         <tr>
-                                            <th style="width: 50px;">#</th>
-                                            <th>商品</th>
+                                            <th scope="col" style="width: 50px;">#</th>
+                                            <th scope="col" class="text-nowrap" style="width: 75px;">圖片</th>
+                                            <th scope="col" class="text-nowrap">商品</th>
                                         </tr>
                                     </thead>
-                                    <?php
+                                    <?php 
                                     $count = 1;
                                     $total = 0;
-                                    ?>
-                                    <?php if (!empty($order_item)) {foreach ($order_item as $item) {?>
+                                     if (!empty($order_item)) {
+                                        foreach ($order_item as $item) {
+                                            if ($item['product_id'] == 0) {?>
                                     <tbody>
                                         <tr>
                                             <td><?php echo $count ?></td>
+                                            <td><?php $this->db->select('*');
+                                                $this->db->from('product_combine');
+                                                $this->db->where('id', $item['product_combine_id']);
+                                                $query = $this->db->get();
+                                                foreach ($query->result_array() as $row) {
+                                                    echo get_front_image($row['picture']);
+                                                }?>
+                                            </td>
                                             <td>
                                                 <div>
-                                                    <?php
-                                                    if($item['product_id']==0){
-                                                        echo get_product_combine_name($item['product_combine_id']);
-                                                    }
-                                                    if($item['product_id']!=0){
-                                                        echo get_product_name($item['product_id']);
-                                                    }
-                                                    ?>
+                                                <?php $i = 0;
+                                                $this->db->select('*');
+                                                $this->db->from('product_combine');
+                                                $this->db->join('product_combine_item', 'product_combine.id = product_combine_item.product_combine_id', 'right');
+                                                $this->db->where('product_combine.id', $item['product_combine_id']);
+                                                $query = $this->db->get();
+                                                foreach ($query->result_array() as $row) {
+                                                    // echo $row['id'] . ' ' . $row['product_specification'] . ' ' . $row['product_id'] . '<br>';
+                                                    if ($i < 1) {
+                                                        echo get_product_name($row['product_id']) . ' - ' . get_product_combine_name($row['product_combine_id']);
+                                                    }?>
+                                                    <ul class="pl-3 m-0" style="color: gray;">
+                                                        <li style="list-style-type: circle;">
+                                                        <?echo $row['qty'] . ' ' . $row['product_unit'];
+                                                        if (!empty($row['product_specification'])) {
+                                                            echo ' - ' . $row['product_specification'];
+                                                        }?>
+                                                        </li>
+                                                    </ul>
+                                                    <?$i++;}?>
                                                 </div>
                                                 <div>金額：$<?php echo number_format($item['order_item_price']) ?></div>
                                                 <div>數量：<?php echo $item['order_item_qty'] ?></div>
-                                                <div>小計：$<?php echo number_format($item['order_item_qty'] * $item['order_item_price']) ?></div>
+                                                <div>小計：<span style="color:#dd0606;">$<?php echo number_format($item['order_item_qty'] * $item['order_item_price']) ?></span></div>
                                             </td>
                                         </tr>
                                     </tbody>
-                                    <?php $count++; ?>
-                                    <?php $total += $item['order_item_qty'] * $item['order_item_price']; ?>
-                                    <?php }} ?>
+                                    <?php $count++;?>
+                                    <?php $total += $item['order_item_qty'] * $item['order_item_price'];?>
+                                    <?php }}}?>
                                 </table>
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                <p>配送方式：</p>
+                                <p class="front_title">配送方式：</p>
                                 <p><?php echo get_delivery($order['order_delivery']) ?></p>
                                 <p>
                                 <?php if(!empty($order['order_store_address'])){
@@ -78,13 +113,13 @@
                         </tr>
                         <tr>
                             <td>
-                                <p>付款方式：</p>
+                                <p class="front_title">付款方式：</p>
                                 <p><?php echo get_payment($order['order_payment']) ?></p>
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                <p>備註：</p>
+                                <p class="front_title">備註：</p>
                                 <p>
                                     <?php echo $order['order_remark']; ?>
                                 </p>
@@ -92,26 +127,17 @@
                         </tr>
                         <tr>
                             <td>
-                                <p>小計：</p>
-                                <p>
-                                    <?php echo number_format($total); ?>
-                                </p>
+                                <span class="front_title">小計：<span class="money_size">$<?php echo number_format($total) ?> </span></span>
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                <p>運費：</p>
-                                <p>
-                                    <?php echo number_format($order['order_delivery_cost']); ?>
-                                </p>
+                                <span class="front_title">運費：<span class="money_size">$<?php echo number_format($order['order_delivery_cost']) ?> </span></span>
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                <p>總計：</p>
-                                <p>
-                                    <?php echo number_format($order['order_discount_total']) ?>
-                                </p>
+                                <span class="front_title">總計：<span class="money_size">$<?php echo number_format($order['order_discount_total']) ?></span></span>
                             </td>
                         </tr>
                     </table>
