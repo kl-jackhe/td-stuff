363cfb81233bc69b3d570ee173559186b49a83ac
azuma0327 <azuma20010327@gmail.com>
2023-12-06 19:00:00 +0800

    小修正

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -4,16 +4,57 @@
         font-weight: bold;
     }
 
-    .breadcrumb {
+    .post_breadcrumb {
         display: flex;
         justify-content: center;
+        -ms-flex-wrap: wrap;
+        flex-wrap: wrap;
+        padding: 0.75rem 1rem;
+        margin-bottom: 1rem;
         list-style: none;
-        /* padding: 0; */
+        border-radius: 0.25rem;
+        position: relative;
     }
 
-    .breadcrumb li {
+    .post_breadcrumb li {
         margin: 0 10px;
-        /* 可根據需要調整按鈕之間的距離 */
+    }
+
+    .post_breadcrumb #post_search {
+        color: rgba(211, 84, 72, 0.5);
+        margin: 0 10px;
+        width: 100%;
+        height: 40px;
+        padding-left: 20px;
+        border-color: rgba(211, 84, 72, 0.5);
+        border-radius: 5px;
+        box-sizing: border-box;
+        transition: all .3s ease-in-out;
+        -moz-transition: all .3s ease-in-out;
+        -webkit-transition: all .3s ease-in-out;
+        -o-transition: all .3s ease-in-out;
+    }
+
+    .post_breadcrumb #post_search:focus {
+        outline: none;
+        box-shadow: rgba(211, 84, 72, 0.5) 0px 3px 8px;
+        border-color: rgba(211, 84, 72, 1);
+    }
+
+    .post_breadcrumb #post_search::placeholder {
+        color: rgba(211, 84, 72, 0.5);
+    }
+
+    .clear-search {
+        position: absolute;
+        right: 45px;
+        top: 20px;
+        cursor: pointer;
+        color: rgba(211, 84, 72, 0.5);
+    }
+
+    .clear-search:hover {
+        color: rgba(211, 84, 72, 1);
     }
 
     .touch_effect {
@@ -83,15 +124,15 @@
         text-decoration: none;
     }
 
-    .btn {
+    .category_btn {
         display: inline-block;
         padding: 10px 20px;
         font-size: 16px;
         text-align: center;
         text-decoration: none;
         cursor: pointer;
-        border: 2px solid #e07f55;
-        color: #e07f55;
+        border: 1px solid #d35448;
+        color: #d35448;
         border-radius: 5px;
         transition: all .3s ease-in-out;
         -moz-transition: all .3s ease-in-out;
@@ -99,10 +140,75 @@
         -o-transition: all .3s ease-in-out;
     }
 
-    .btn.active,
-    .btn:hover {
+    .category_btn.active,
+    .category_btn:hover,
+    .category_btn:checked {
         background-color: #d35448;
+        box-shadow: rgba(211, 84, 72, 0.5) 0px 3px 8px;
         color: #fff;
+    }
+
+    .post_page_link {
+        position: relative;
+        display: block;
+        padding: 0.5rem 0.75rem;
+        margin-left: -1px;
+        line-height: 1.25;
+        color: #d35448;
+        background-color: #fff;
+        border: 1px solid #dee2e6;
+        transition: all .3s ease-in-out;
+        -moz-transition: all .3s ease-in-out;
+        -webkit-transition: all .3s ease-in-out;
+        -o-transition: all .3s ease-in-out;
+    }
+
+    .post_page_link:hover {
+        z-index: 2;
+        color: #fff;
+        text-decoration: none;
+        background-color: #d35448;
+        border-color: #dee2e6;
+    }
+
+    .post_page_link:focus {
+        z-index: 2;
+        outline: 0;
+        box-shadow: 0 0 0 0.2rem rgba(211, 84, 30, .25);
+    }
+
+    .page-item.disabled .post_page_link {
+        color: #6c757d;
+        pointer-events: none;
+        cursor: auto;
+        background-color: #fff;
+        border-color: #dee2e6;
+    }
+
+    .page-item:first-child .post_page_link {
+        margin-left: 0;
+        border-top-left-radius: 0.25rem;
+        border-bottom-left-radius: 0.25rem;
+    }
+
+    .page-item:last-child .post_page_link {
+        margin-left: 0;
+        border-top-right-radius: 0.25rem;
+        border-bottom-right-radius: 0.25rem;
+    }
+
+    .post_pagination {
+        margin-top: 30px;
+        text-align: center;
+        padding-left: 0;
+        list-style: none;
+        border-radius: 0.25rem rem;
+    }
+
+    .post_pagination li {
+        display: inline-block;
+        margin-right: 5px;
+        /* 設定按鈕之間的間距，可根據需求調整 */
     }
 </style>
 
@@ -111,21 +217,42 @@
 <div id="app" role="main" class="main pt-signinfo">
     <section id="post_rejust">
         <div class="container">
-            <div class="crumb">
-                <ul class="breadcrumb" itemscope="" itemtype="http://schema.org/BreadcrumbList">
+            <div class="post_breadcrumb">
+                <input type="text" id="post_search" placeholder="搜尋欄" v-model="searchText">
+                <span v-if="searchText !== ''" @click="clearSearch" class="clear-search"><i class="fa fa-times" aria-hidden="true"></i></span>
+            </div>
+            <div v-if="searchText === ''" class="crumb">
+                <ul class="post_breadcrumb" itemscope="" itemtype="http://schema.org/BreadcrumbList">
                     <li v-for="category in categories" :key="category.post_category_id">
-                        <input type="button" :value="category.post_category_name" @click="filterByCategory(category.post_category_id)" :class="{ btn: true, active: selectedCategoryId === category.post_category_id}">
+                        <input type="button" :value="category.post_category_name" @click="filterByCategory(category.post_category_id)" :class="{ category_btn: true, active: selectedCategoryId === category.post_category_id}">
                     </li>
                 </ul>
             </div>
         </div>
         <div class="section-contents">
             <div class="container">
-                <h1><span>最新訊息</span></h1>
+                <h1><span>{{ pageTitle }}</span></h1>
             </div>
             <div class="container">
-                <!-- 這裡顯示你的文章內容 -->
-                <div v-for="self in filteredPosts" :key="self.post_id">
+                <!-- Pagination -->
+                <ul v-if="totalPages !== 0" class="post_pagination">
+                    <li class="page-item" :class="{'disabled': currentPage === 1}" @click.prevent="setPage(currentPage-1)">
+                        <a class="post_page_link" href="" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li v-for="n in limitedPages" :key="n" :class="{'active': (currentPage === n)}" @click.prevent="setPage(n)">
+                        <a class="post_page_link" href="">{{ n }}</a>
+                    </li>
+                    <li class="page-item" :class="{'disabled': (currentPage === totalPages) || (totalPages === 0)}" @click.prevent="setPage(currentPage+1)">
+                        <a class="post_page_link" href="" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <!-- Post Start -->
+                <div v-for="self in filteredPosts.slice(pageStart, pageEnd)" :key="self.post_id">
                     <a class="font_color" :href="'/posts/view/' + self.post_id">
                         <div class="row touch_effect">
                             <div class="col-md-3 offset-md-1 text-center">
@@ -139,8 +266,25 @@
                             </div>
                         </div>
                     </a>
-                    <!-- Post End -->
                 </div>
+                <!-- Post End -->
+
+                <!-- Pagination -->
+                <ul v-if="totalPages !== 0" class="post_pagination">
+                    <li class="page-item" :class="{'disabled': currentPage === 1}" @click.prevent="setPage(currentPage-1)">
+                        <a class="post_page_link" href="" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li v-for="n in limitedPages" :key="n" :class="{'active': (currentPage === n)}" @click.prevent="setPage(n)">
+                        <a class="post_page_link" href="">{{ n }}</a>
+                    </li>
+                    <li class="page-item" :class="{'disabled': (currentPage === totalPages) || (totalPages === 0)}" @click.prevent="setPage(currentPage+1)">
+                        <a class="post_page_link" href="" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </section>
@@ -150,26 +294,90 @@
     const app = Vue.createApp({
         data() {
             return {
-                categories: <?php echo json_encode($posts_category); ?>,
-                selectedCategoryId: 1,
-                posts: <?php echo json_encode($posts); ?>,
+                categories: <?php echo json_encode($posts_category); ?>, // posts_category資料庫所有類及項目
+                selectedCategoryId: 1, // 目前顯示頁面主題, 1為最新消息
+                posts: <?php echo json_encode($posts); ?>, // posts資料庫所有類及項目
+                pageTitle: "最新訊息", // 目前標籤
+                perpage: 5, // 一頁的資料數
+                currentPage: 1, // 目前page
+                searchText: '', // 搜尋欄
             };
+        },
+        mounted() {
+            // 在 mounted 鉤子中設定 selectedCategoryId 的值
+            if (this.categories.length > 0) {
+                this.selectedCategoryId = this.categories[0].post_category_id;
+            }
         },
         computed: {
             filteredPosts() {
+                if (this.searchText.trim() !== '') {
+                    this.pageTitle = "搜尋結果"
+                    return this.filterPostsBySearch();
+                } else {
+                    if (this.pageTitle === "搜尋結果") {
+                        this.clearSearch();
+                    }
+                    return this.filterPostsByCategory();
+                }
+            },
+            limitedPages() {
+                const maxPages = 5;
+                const middlePage = Math.ceil(maxPages / 2);
+
+                if (this.totalPages <= maxPages) {
+                    return Array.from({
+                        length: this.totalPages
+                    }, (_, i) => i + 1);
+                } else if (this.currentPage <= middlePage) {
+                    return Array.from({
+                        length: maxPages
+                    }, (_, i) => i + 1);
+                } else if (this.currentPage > this.totalPages - middlePage) {
+                    return Array.from({
+                        length: maxPages
+                    }, (_, i) => this.totalPages - maxPages + i + 1);
+                } else {
+                    return Array.from({
+                        length: maxPages
+                    }, (_, i) => this.currentPage - middlePage + i + 1);
+                }
+            },
+            totalPages() {
+                return Math.ceil(this.filteredPosts.length / this.perpage);
+            },
+            pageStart() {
+                return (this.currentPage - 1) * this.perpage
+                //取得該頁第一個值的index
+            },
+            pageEnd() {
+                const end = this.currentPage * this.perpage;
+                return Math.min(end, this.filteredPosts.length);
+                //取得該頁最後一個值的index
+            }
+        },
+        methods: {
+            filterPostsBySearch() {
+                return this.posts.filter(post => {
+                    const titleMatch = post.post_title.toLowerCase().includes(this.searchText.toLowerCase());
+                    const contentMatch = post.post_content.toLowerCase().includes(this.searchText.toLowerCase());
+                    return titleMatch || contentMatch;
+                });
+            },
+            filterPostsByCategory() {
                 if (this.selectedCategoryId == 1) {
                     return this.posts;
                 } else {
-                    return this.posts.filter(self => self.post_category === this.selectedCategoryId);
+                    return this.posts.filter(post => post.post_category === this.selectedCategoryId);
                 }
             },
-        },
-        methods: {
             filterByCategory(categoryId) {
                 this.selectedCategoryId = categoryId;
                 // 檢查過濾後的文章
                 // console.log('Selected Category ID:', categoryId);
                 // console.log('Filtered Posts:', this.filteredPosts);
+                const selectedCategory = this.categories.find(category => category.post_category_id === categoryId);
+                this.pageTitle = selectedCategory ? selectedCategory.post_category_name : "最新訊息";
             },
             customExcerpt(content, maxLength) {
                 // 使用 DOMParser 解析 HTML 字符串
@@ -185,8 +393,25 @@
 
                 return truncatedText;
             },
+            setPage(page) {
+                if (page <= 0 || page > this.totalPages || (page === this.totalPages && this.currentPage === this.totalPages)) {
+                    return;
+                }
+                this.currentPage = page;
+
+                // 將頁面滾動到頂部
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth' // 若要有平滑的滾動效果
+                });
+            },
+            clearSearch() {
+                this.searchText = '';
+                // this.pageTitle = "最新訊息";
+                // this.selectedCategoryId = 1;
+                this.filterByCategory(1); // 在清除搜尋欄後自動執行第一個篩選
+            },
         },
     });
-
     app.mount('#app');
 </script>
