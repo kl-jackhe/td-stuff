6c70fb5de49b876163d1df6617a3a7a22ff00420
azuma0327 <azuma20010327@gmail.com>
2023-12-09 19:54:12 +0800

    篩選

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,17 +1,27 @@
 <div id="postApp" role="main" class="main pt-signinfo">
     <section id="post_rejust">
         <div class="searchContainer container">
+            <!-- 篩選清單呼叫鈕 -->
             <div class="left-content">
-                <span id="menu-btn" @click="toggleNav" :class="{ 'active': isBtnActive }"><i class="fa fa-bars" aria-hidden="true"></i></span>
+                <span v-if="!isNavOpen" id="menu-btn" @click="toggleNav">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </span>
+                <span v-else id="menu-btn" :class="{ 'active': isBtnActive }" @click="toggleNav">
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                </span>
             </div>
+            <!-- 篩選清單呼叫鈕 -->
+
+            <!-- 搜尋攔 -->
             <div class="right-content breadcrumb">
                 <input type="text" class="search" placeholder="搜尋欄" v-model="searchText">
                 <span v-if="searchText !== ''" @click="clearSearch" class="clear-search"><i class="fa fa-times" aria-hidden="true"></i></span>
             </div>
+            <!-- 搜尋攔 -->
         </div>
         <div :class="{ 'section-sidemenu': true, 'nav-open': isNavOpen }">
             <h1 class=""><span>最新消息</span></h1>
-            <!-- menu-main為第一層選單,menu-sub為第二層選單,menu-grand為第三層選單 -->
+            <!-- 篩選清單 -->
             <ul v-if="searchText === ''" class="menu-main">
                 <li v-for="category in posts_categorys" :key="category.post_category_id">
                     <input type="button" :value="'>&nbsp;' + category.post_category_name" @click="filterByCategory(category.post_category_id)" :class="{ category_btn: true, active: selectedCategoryId === category.post_category_id}">
@@ -22,6 +32,7 @@
                     <input type="button" value=">&nbsp;搜尋結果" :class="{ category_btn: true, active: true}">
                 </li>
             </ul>
+            <!-- 篩選清單 -->
         </div>
         <div class="section-contents">
             <div class="container">
@@ -122,8 +133,8 @@
                 currentPage: 1, // 目前page
                 searchText: '', // 搜尋欄
                 isNavOpen: false, // nav搜尋標籤初始狀態為關閉
-                isBtnActive: false, // nav-btn active state
                 sharedData: null, // 測試footer&header篩選
+                isBtnActive: false, // nav-btn active state
             };
         },
         mounted() {
--- application/views/product/index.php
+++ application/views/product/index.php
@@ -1,17 +1,27 @@
 <div id="productApp" role="main" class="main pt-signinfo">
     <section id="product_rejust">
         <div class="searchContainer container">
+            <!-- 篩選清單呼叫鈕 -->
             <div class="left-content">
-                <span id="menu-btn" @click="toggleNav" :class="{ 'active': isBtnActive }"><i class="fa fa-bars" aria-hidden="true"></i></span>
+                <span v-if="!isNavOpen" id="menu-btn" @click="toggleNav">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </span>
+                <span v-else id="menu-btn" :class="{ 'active': isBtnActive }" @click="toggleNav">
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                </span>
             </div>
+            <!-- 篩選清單呼叫鈕 -->
+
+            <!-- 搜尋攔 -->
             <div class="right-content breadcrumb">
                 <input type="text" class="search" placeholder="搜尋欄" v-model="searchText">
                 <span v-if="searchText !== ''" @click="clearSearch" class="clear-search"><i class="fa fa-times" aria-hidden="true"></i></span>
             </div>
+            <!-- 搜尋攔 -->
         </div>
         <div :class="{ 'section-sidemenu': true, 'nav-open': isNavOpen }">
             <h1 class=""><span>夥伴商城</span></h1>
-            <!-- menu-main為第一層選單,menu-sub為第二層選單,menu-grand為第三層選單 -->
+            <!-- 篩選清單 -->
             <ul v-if="searchText === ''" class="menu-main">
                 <li v-for="category in products_categories" :key="category.product_category_id">
                     <input type="button" :value="'>&nbsp;' + category.product_category_name" @click="filterByCategory(category.product_category_id)" :class="{ category_btn: true, active: selectedCategoryId === category.product_category_id}">
@@ -22,6 +32,7 @@
                     <input type="button" value=">&nbsp;搜尋結果" :class="{ category_btn: true, active: true}">
                 </li>
             </ul>
+            <!-- 篩選清單 -->
         </div>
         <div class="section-contents">
             <div class="container">
