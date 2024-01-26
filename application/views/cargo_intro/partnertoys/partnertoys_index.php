d4a505618340c799f7fc0af907f1c337879773db
azuma0327 <azuma20010327@gmail.com>
2024-01-26 17:54:48 +0800

    add artist href to specific cargo_intro

--- application/views/cargo_intro/partnertoys/partnertoys_index.php
+++ application/views/cargo_intro/partnertoys/partnertoys_index.php
@@ -38,15 +38,15 @@
     const cargoApp = Vue.createApp({
         data() {
             return {
-                getID: <?php echo json_encode($this->input->get('id', TRUE)); ?>, // 若透過header或footer篩選
-                getYear: <?php echo json_encode($this->input->get('year', TRUE)); ?>, // 若透過合作指引
-                getCargoName: <?php echo json_encode($this->input->get('name', TRUE)); ?>, // 若透過合作指引
+                getID: <?php echo json_encode((!empty($this->input->get('id', TRUE)) ? $this->input->get('id', TRUE) : '')); ?>, // 若透過header或footer篩選
+                getYear: <?php echo json_encode((!empty($this->input->get('year', TRUE)) ? $this->input->get('year', TRUE) : '')); ?>, // 若透過合作指引
+                getCargoName: <?php echo json_encode((!empty($this->input->get('name', TRUE)) ? $this->input->get('name', TRUE) : '')); ?>, // 若透過合作指引
                 selectedCategoryId: null, // 目前顯示頁面主題
                 selectedSubCategoryId: null,
                 selectedSubSubCategoryId: null,
                 cargo_category: <?php echo json_encode(!empty($cargo_category) ? $cargo_category : ''); ?>,
-                cargo_son_category: null,
-                cargo_sub_son_category: null,
+                cargo_son_category: [],
+                cargo_sub_son_category: [],
                 pageTitle: '', // 目前標籤
                 isNavOpen: false, // nav搜尋標籤初始狀態為關閉
                 isBtnActive: false, // nav-btn active state
@@ -57,15 +57,22 @@
         mounted() {
             // init btn state
             if (this.cargo_category && this.cargo_category.length > 0) {
-                if (this.getID && this.getID.length > 0) {
-                    this.selectedCategoryId = this.getID;
-                    this.toggleCategory(this.cargo_category[parseInt(this.getID) - 1].id);
-                    const tmpSet = this.cargo_category.find(self => self.sort === this.getID);
-                    this.pageTitle = tmpSet.name;
+                if (this.getID.length > 0) {
+                    if (this.getYear.length > 0 && this.getCargoName.length > 0) {
+                        // artist href
+                        // console.log('ID:' + this.getID);
+                        // console.log('Year:' + this.getYear);
+                        // console.log('Name:' + this.getCargoName);
+                        this.hrefToggleCategory(this.cargo_category[parseInt(this.getID) - 1].id)
+                    } else {
+                        // header category
+                        // console.log('ID:' + this.getID);
+                        this.selectedCategoryId = this.getID;
+                        this.toggleCategory(this.cargo_category[parseInt(this.getID) - 1].id);
+                    }
                 } else {
                     this.selectedCategoryId = this.cargo_category[0].sort;
                     this.toggleCategory(this.cargo_category[0].id);
-                    this.pageTitle = this.cargo_category[0].name;
                 }
             }
         },
@@ -125,6 +132,55 @@
                         // 更新選擇類別
                         this.selectedSubCategoryId = this.isSubExpanded.sort;
                         this.pageTitle = this.isSubExpanded.name;
+                    }
+                })
+            },
+            hrefToggleCategory(categoryId) {
+                this.selectedCategoryId = this.getID;
+                $.ajax({
+                    url: '/cargo_intro/selected_son/' + categoryId,
+                    method: 'post',
+                    success: (response) => {
+                        this.cargo_son_category = response;
+                        // 確定上一個選擇類別決定開關狀態
+                        this.isExpanded = this.cargo_category.find(self => self.id === categoryId);
+                        if (this.isExpanded.sort != this.selectedCategoryId || this.selectedSubCategoryId !== null) {
+                            this.isExpanded.switch = true;
+                        } else {
+                            this.isExpanded.switch = !this.isExpanded.switch;
+                        }
+                        // 當點回最上層將其他層關閉
+                        this.selectedSubCategoryId = null;
+                        this.selectedSubSubCategoryId = null;
+                        this.isSubExpanded = [];
+                        // 更新選擇類別
+                        this.selectedCategoryId = this.isExpanded.sort;
+                        this.pageTitle = this.isExpanded.name;
+
+                        // 找指定年
+                        const tmp_year = this.cargo_son_category.find(self => self.name == this.getYear);
+                        $.ajax({
+                            url: '/cargo_intro/selected_sub_son/' + tmp_year.id,
+                            method: 'post',
+                            success: (response) => {
+                                this.cargo_sub_son_category = response;
+                                // 確定上一個選擇類別決定開關狀態
+                                this.isSubExpanded = this.cargo_son_category.find(self => self.id === tmp_year.id);
+                                if (this.isSubExpanded.sort != this.selectedSubCategoryId || this.selectedSubSubCategoryId !== null) {
+                                    this.isSubExpanded.switch = true;
+                                } else {
+                                    this.isSubExpanded.switch = !this.isSubExpanded.switch;
+                                }
+                                this.selectedSubSubCategoryId = null;
+                                // 更新選擇類別
+                                this.selectedSubCategoryId = this.isSubExpanded.sort;
+                                this.pageTitle = this.isSubExpanded.name;
+
+                                // 找指定名稱
+                                const tmp_name = this.cargo_sub_son_category.find(self => self.name == this.getCargoName);
+                                this.filterBySubSubCategory(tmp_name.id);
+                            }
+                        })
                     }
                 })
             },
